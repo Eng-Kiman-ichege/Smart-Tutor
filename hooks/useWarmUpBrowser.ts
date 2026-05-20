0cf51e97e3e9e6bc71193f4226142b5e1722b25f
Evan <evan@Evans-MacBook-Pro.local>
2026-05-20 14:42:36 +0300

    authentication

--- hooks/useWarmUpBrowser.ts
+++ hooks/useWarmUpBrowser.ts
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
 
 export const useWarmUpBrowser = () => {
   React.useEffect(() => {
     // Warm up the browser for faster OAuth loads (especially on Android)
-    void WebBrowser.warmUpAsync();
-    return () => {
-      void WebBrowser.coolDownAsync();
-    };
+    if (Platform.OS !== 'web') {
+      void WebBrowser.warmUpAsync();
+      return () => {
+        void WebBrowser.coolDownAsync();
+      };
+    }
   }, []);
 };
